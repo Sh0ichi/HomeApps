e1c3ad806e195baaf06d3d48049362ceb3ed2a31
Sh0ichi <rainbow.sheep.poro@gmail.com>
2017-08-10 04:21:32 +0900

    view expens

--- lib/index_month.rb
+++ lib/index_month.rb
@@ -1,9 +1,10 @@
 class IndexMonth
   include ActiveModel::Model
-  attr_accessor :record_month, :income
+  attr_accessor :record_month, :income, :expens
 
   def initialize(record_month)
     @record_month = record_month
     @income = BigDecimal.new(0)
+    @expens = BigDecimal.new(0)
   end
 end
